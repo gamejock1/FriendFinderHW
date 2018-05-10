dc72dcf20e3f431a8b186e3db497ba415da7d70e
John yeager <johnyeager@johns-mbp.home>
2018-05-10 12:53:39 -0400

    made file structure

---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
